f50e6b30a2974153ac5839b1fc48c0c3c26ead70
Gustav Jakobsson <gustav.jakobsson@abbgymnasiet.se>
2023-10-05 14:03:36 +0200

    Extend path

--- playground/metadata.py
+++ playground/metadata.py
@@ -1,6 +1,11 @@
+import sys
+
+sys.path.append('../fleet-learning-young-talents')
+
 from zod import ZodFrames
 from zod import constants
 from data_partitioner import partition_train_data, PartitionStrategy
+from data_loader import load_datasets
 import matplotlib.pyplot as plt
 
 NO_CLIENTS = 40
@@ -13,6 +18,8 @@ def main() -> None:
         zod_frames,
         1.0
     )
+    data = load_datasets(partitions, zod_frames)
+    print(data)
 
 if __name__ == '__main__':
     main()
