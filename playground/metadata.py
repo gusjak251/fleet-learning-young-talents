12178b645d1d6caa40f4e8b5478f1dc8a22381c3
gusjak251 <gustavjakobsson3@gmail.com>
2023-10-11 11:55:29 +0000

    playground: load metadata

--- playground/metadata.py
+++ playground/metadata.py
@@ -7,11 +7,19 @@ from zod import constants
 from data_partitioner import partition_train_data, PartitionStrategy
 from data_loader import load_datasets
 import matplotlib.pyplot as plt
+import json
 
 NO_CLIENTS = 40
 
 def main() -> None:
     zod_frames = ZodFrames("/mnt/ZOD", version="full")
+    for i in range(20):
+        frame = zod_frames[str(i)]
+        file = open(frame.info.metadata_path)
+        metadata = json.load(file)
+        print(metadata)
+    # metadata = json.load(metadata_path)
+    exit()
     partitions = partition_train_data(
         PartitionStrategy.RANDOM,
         NO_CLIENTS,
